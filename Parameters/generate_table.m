3c280b49f3987a6b2725385681a774fe768c562b
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2019-11-25 17:20:59 +0000

    generate_table.m modified to make table compatible with simulation

--- Parameters/generate_table.m
+++ Parameters/generate_table.m
@@ -31,12 +31,13 @@ forces      = data(1:end,3);
 forces = reshape(forces,length(velocities),length(frequencies));
 
 % Save Workspace
-save(out_table)
+save(out_table,'forces','v_step','freq_step','velocities','frequencies')
 
 % Plot mesh
-f = figure('Name','Forces plot','NumberTitle','off');
+f = figure('Name','Forces plot');
+ax= axes('Parent',f)
 
-mesh(frequencies,velocities,forces);
+mesh(ax,frequencies,velocities,forces);
 title('Forces look-up table');
 xlabel('Frequency (Hz)');
 ylabel('Velocity (ms^{-1})');
@@ -46,3 +47,35 @@ zlabel('Thrust force (N)');
 savefig(f,out_fig)
 fprintf("Workspace saves as:\t%s\nFigure saved as:\t%s\n",...
         out_table,out_fig);
+    
+    
+%% Determine optimal slip as a function of velocity
+
+
+
+opt_frequency   = velocities;
+opt_forces      = zeros(length(velocities),1);
+
+for i = 1:length(velocities)
+    [~,pos] = max(forces(i,:));
+    opt_frequency(i) = frequencies(pos);
+    opt_forces(i)    = forces(i,pos);
+end
+%
+hold on
+scatter3(ax,opt_frequency,velocities,opt_forces)
+hold off
+
+f2=figure('Name','Optimal Frequency')
+scatter(velocities,opt_frequency)
+
+[coeffs,ds] = polyfit(velocities,opt_frequency,3)
+
+fit = polyval(coeffs,velocities);
+hold on 
+plot(velocities,fit);
+hold off
+
+title('Optimal frequency')
+xlabel('Velocity (ms^{-1})')
+ylabel('Frequency (Hz)')
